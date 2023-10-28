156cbac3fe63e3a6b30a397549380012b4c47c3d
Vishal Sharma <vishalsharma34256@gmail.com>
2023-10-28 18:19:26 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-      </Routes>
-    </BrowserRouter>
-  );
+  return <Login />;
 }
 
 export default App;
